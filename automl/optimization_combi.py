35cf6d5e2ba4e9683a60a27c44937c59a3a3cc84
MingCai06 <ming.cai06@gmail.com>
2019-09-01 17:10:02 +0200

    add new parameter to eable extern regression

--- automl/optimization_combi.py
+++ automl/optimization_combi.py
@@ -1,38 +1,43 @@
 # Importing core libraries
 import time
 import pprint
-import joblib
 import pandas as pd
+import numpy as np
+
+from feature_selection import feature_selector
+from model import Classifier
+from scaling import Scaler
+from encoder import Categorical_encoder
+from util import log, timeit, mprint, dump_result, load_result
+from bayessearch_custom import BayesSearchCV
+
+from skopt.callbacks import DeltaYStopper
+from skopt.callbacks import VerboseCallback  # Callback to control the verbosity
+from skopt.callbacks import DeadlineStopper
+from skopt.learning import GaussianProcessRegressor
+from skopt.utils import use_named_args
+from skopt.space import Real, Categorical, Integer
+
+from sklearn.metrics import make_scorer
+from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import StratifiedKFold
+
 
 # Suppressing warnings because of skopt verbosity
 import warnings
 warnings.filterwarnings("ignore")
 
 # Model selection
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 
 # Metrics
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import make_scorer
 
 # Skopt functions
-#from skopt import BayesSearchCV
-from skopt.space import Real, Categorical, Integer
-from skopt.utils import use_named_args  # decorator to convert a list of parameters to named arguments
-from skopt.callbacks import DeadlineStopper  # Stop the optimization before running out of a fixed budget of time.
-from skopt.callbacks import VerboseCallback  # Callback to control the verbosity
-from skopt.callbacks import DeltaYStopper  # Stop the optimization If the last two positions at which the objective has been evaluated are less than delta
+# from skopt import BayesSearchCV
 
 # import local function
-from bayessearch_custom import BayesSearchCV
-from util import log, timeit, mprint, dump_result, load_result
-from encoder import Categorical_encoder
-from scaling import Scaler
-from model import Classifier
-from feature_selection import feature_selector
 
 
 class automl_Optimiser():
@@ -57,8 +62,8 @@ class automl_Optimiser():
     Return
     ----------
     final_result : dict, result for all surrogated models
-        - final_result['GP'] 
-        - final_result['RF'] 
+        - final_result['GP']
+        - final_result['RF']
     ----------
     Example
     ----------
@@ -70,7 +75,8 @@ class automl_Optimiser():
     df_train = data['train'] #df_test = data['test']
     y = data['target'] #y_test = data['y_test']
     search_space_LGB = Classifier(strategy = "LightGBM").get_search_spaces()
-    search_space_RF  = Classifier(strategy = "RandomForest").get_search_spaces()
+    search_space_RF  = Classifier(
+        strategy = "RandomForest").get_search_spaces()
 
     best_param,results = optimiser.optimise_step(space = [(search_space_LGB_fs,5),(search_space_RF,5)],
                                 df_train=X_train,
@@ -85,7 +91,9 @@ class automl_Optimiser():
                  verbose=True,
                  to_path="save",
                  perform_scaling=True,
-                 parallel_strategy=True):
+                 parallel_strategy=True,
+                 refit=True,
+                 baseEstimator=["GP", "RF"]):
 
         self.scoring = scoring
         self.n_folds = n_folds
@@ -94,15 +102,19 @@ class automl_Optimiser():
         self.to_path = to_path
         self.perform_scaling = False
         self.parallel_strategy = parallel_strategy
+        self.refit = refit
+        self.baseEstimator = baseEstimator
 
         if self.to_path is True:
             warnings.warn("Optimiser will save all your fitted models result ")
 
-        self.cv = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.random_state)
+        self.cv = StratifiedKFold(
+            n_splits=self.n_folds, shuffle=True, random_state=self.random_state)
 
         # Default scoring for classification
         if (self.scoring is None):
-            self.scoring = make_scorer(roc_auc_score, greater_is_better=True, needs_threshold=True)
+            self.scoring = make_scorer(
+                roc_auc_score, greater_is_better=True, needs_threshold=True)
 
         elif (self.scoring == "log_loss"):
             self.scoring = 'log_loss'
@@ -226,13 +238,15 @@ class automl_Optimiser():
                     pass
                 mprint(f'Start turning Hyperparameters .... ')
                 print("")
-                print(">>> Categorical Features have encoded with :" + str({'strategy': ce.strategy}))
+                print(">>> Categorical Features have encoded with :" +
+                      str({'strategy': ce.strategy}))
                 print("")
                 if self.perform_scaling is True:
-                    print(">>> Numerical Features have encoded with :" + scal.__class__.__name__)
+                    print(">>> Numerical Features have encoded with :" +
+                          scal.__class__.__name__)
                     print("")
 
-                for baseEstimator in ['GP', 'RF']:
+                for baseestimator in self.baseEstimator:
                     # Pipeline creation
 
                     lgb = Classifier(strategy="LightGBM").get_estimator()
@@ -241,12 +255,14 @@ class automl_Optimiser():
 
                     if (fs is not None):
                         if cache:
-                            pipe = Pipeline([('fs', fs), ('model', lgb)], memory=self.to_path)
+                            pipe = Pipeline(
+                                [('fs', fs), ('model', lgb)], memory=self.to_path)
                         else:
                             pipe = Pipeline([('fs', fs), ('model', lgb)])
                     else:
                         if cache:
-                            pipe = Pipeline([('model', lgb)], memory=self.to_path)
+                            pipe = Pipeline([('model', lgb)],
+                                            memory=self.to_path)
                         else:
                             pipe = Pipeline([('model', lgb)])
 
@@ -259,10 +275,11 @@ class automl_Optimiser():
                                             n_jobs=-1,
                                             nrandom=nrandom,
                                             return_train_score=False,
-                                            optimizer_kwargs={'base_estimator': baseEstimator,
+                                            optimizer_kwargs={'base_estimator': baseestimator,
                                                               "acq_func": "EI"},
                                             random_state=self.random_state,
-                                            verbose=self.verbose)
+                                            verbose=self.verbose,
+                                            refit=self.refit)
                     else:
                         opt = BayesSearchCV(pipe,
                                             search_spaces=search_spaces,
@@ -272,20 +289,29 @@ class automl_Optimiser():
                                             n_jobs=1,
                                             nrandom=nrandom,
                                             return_train_score=False,
-                                            optimizer_kwargs={'base_estimator': baseEstimator,
+                                            optimizer_kwargs={'base_estimator': baseestimator,
                                                               "acq_func": "EI"},
                                             random_state=self.random_state,
-                                            verbose=self.verbose)
+                                            verbose=self.verbose,
+                                            refit=self.refit)
 
-                    if set_callbacks is True:
+                    if not isinstance(baseestimator, GaussianProcessRegressor):
+                        if set_callbacks is True:
+                            mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model:' + baseestimator,
+                                                          callbacks=[self.on_step, DeadlineStopper(60 * 10)  # ,DeltaYStopper(0.000001)
+                                                                     ])
+                        else:
+                            mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model: ' + baseestimator,)
+                        tuning_result[baseestimator] = mid_result
 
-                        mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model:' + baseEstimator,
-                                                      callbacks=[DeltaYStopper(0.0001), DeadlineStopper(60 * 5)
-                                                                 ])
                     else:
-                        mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model: ' + baseEstimator,
-                                                      )
-                    tuning_result[baseEstimator] = mid_result
+                        if set_callbacks is True:
+                            mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model:' + baseestimator.__class__.__name__,
+                                                          callbacks=[self.on_step, DeadlineStopper(60 * 10)  # ,DeltaYStopper(0.000001)
+                                                                     ])
+                        else:
+                            mid_result = self.report_perf(opt, X, df_target, ' with Surrogate Model: ' + baseestimator.__class__.__name__,)
+                        tuning_result[baseestimator.__class__.__name__] = mid_result
 
         bests = pd.DataFrame()
         for key in tuning_result.keys():
@@ -300,7 +326,10 @@ class automl_Optimiser():
         print("")
         print('######## Congratulations! Here is the Best Parameters: #######')
         print('Best Score is:', tuning_result[best_base_estimator]['best_score'])
-        print('with Surrogate Model ' + best_base_estimator)
+        try:
+            print('with Surrogate Model ' + best_base_estimator)
+        except:
+            print('with Surrogate Model ' + best_base_estimator.__class__.__name__)
         pprint.pprint(best_param)
         return best_param, tuning_result
 
@@ -311,6 +340,12 @@ class automl_Optimiser():
     #     if score >= 0.99:
     #         print('Best Score >0.99,Interrupting!')
     #         return True
+    def on_step(self, opt):
+        scores = np.sort(opt.func_vals)
+        score = scores[0]
+        # print("best score: %s" % score)
+        if score == -1:
+            return True
 
     # Reporting util for different optimizers
     def report_perf(self, optimizer, X, y, title, callbacks=None):
